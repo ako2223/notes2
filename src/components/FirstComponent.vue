b82ed22be807224667ca31fc62abbb7edb678146
ako2223 <akoval22@proton.me>
2024-02-17 16:36:54 +0600

    First commit

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+alert('Hello from FirstComponent.vue')
+
+</script>
+
+<template>
+    <div class="card">
+        <div>
+        <img src="../assets/images/panda.jpg" alt="panda_image">
+    </div>
+    
+    <div>
+        <h3>Panda</h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum, sint?</p>
+    </div>
+    </div>
+</template>
+
+<style src="../assets/css/components/firstComponent.css"></style>
